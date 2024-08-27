dda8f2f795f13fcee7c45607a2caaf541454fdc5
shivam98nitt <shivamsingh24680@gmail.com>
2024-08-27 19:34:40 +0530

    landing page responsive , unused png,jpg are removed , certificate are added ,

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,8 +14,8 @@ const Home = () => {
   const carouselItems = [
     {
       img: banner2,
-      title: "Transforming Businesses using AI",
-      text: "Unlock the power of AI and digital transformation with Uno.AI Solutions. We provide expert consulting, innovative technology solutions, and staffing services to help businesses thrive in the digital age.",
+      title: "Empower Your Business with AI",
+      text: "Unlock AI and digital transformation with Uno.AI Solutions for expert consulting, tech solutions, and staffing.",
       buttonText: "Contact Us"
     },
     {
@@ -46,25 +46,27 @@ const Home = () => {
                   backgroundImage: `url(${item.img})`,
                 }}
               >
-                {/* Full-width Blurred Background Container */}
-                <div className="absolute inset-0 mb-20 mx-10 flex flex-col justify-end">
-                  <div className="relative w-full bg-black/40 backdrop-blur-md px-4 pt-1 pb-4 rounded">
-                    <div className="w-full mx-auto ">
-                      <h1 className="text-5xl mb-4 font-semibold text-white leading-snug">
-                        {item.title} <span className="text-brandPrimary leading-snug"></span>
+                <div className="absolute inset-0 mb-6 md:mb-20 mx-4 md:mx-10 flex flex-col justify-end">
+                  <div className="relative w-full bg-black/40 backdrop-blur-md px-4 pt-4 md:pt-2 pb-6 md:pb-4 rounded">
+                    <div className="w-full mx-auto">
+                      <h1 className="text-bold sm:text-3xl md:text-5xl mb-4 font-semibold text-white leading-snug">
+                        {item.title}
+                        <span className="text-brandPrimary leading-snug"></span>
                       </h1>
-                      <p className="text-white text-base mb-8">
+                      <p className="text-xs sm:text-sm md:text-base lg:text-lg text-white mb-4 md:mb-6">
                         {item.text}
                       </p>
                       <button
                         onClick={handleOpenContact}
-                        className="px-7 py-2 bg-brandPrimary text-white rounded hover:bg-neutralDGrey"
+                        className="px-4 sm:px-5 md:px-6 py-2 bg-brandPrimary text-white rounded hover:bg-neutralDGrey"
                       >
                         {item.buttonText}
                       </button>
                     </div>
                   </div>
                 </div>
+
+
               </div>
             </div>
           ))}
