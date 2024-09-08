8e3a17aaeeb3bf66908ae762d3a4d614d692049f
UnoDev1 <159614858+UnoDev1@users.noreply.github.com>
2024-09-08 20:25:28 +0530

    Update AboutUs.jsx

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -6,19 +6,20 @@ const AboutUs = () => {
     <div id="aboutus">
       <h1> <b> About Us </b> </h1>
       <p>
-        With over 30 years of consulting experience, our company delivers a diverse portfolio of expertise tailored to Federal Government, Healthcare, Retail, 
-        and Automobile industry clients. Our passion for technology, innovation, and commitment to continuous learning are key drivers to our success.
+        With over 30 years of consulting experience, our company delivers a diverse portfolio of expertise tailored to Federal Government, Healthcare, Retail, and Automobile industry clients. Our passion for technology, innovation, and commitment to continuous learning are key drivers to our success.
+      </p>
+      <p>
         We offer end-to-end solutions to our clients and specialize in:
-
-          ·Providing telecommunication and emerging wireless communication solutions
-          ·Offering digital transformation services to modernize and enhance operations
-          ·Assisting organizations in implementing Artificial Intelligence for competitive advantage
-          ·Deploying cybersecurity solutions to safeguard critical business data
-          ·Developing and implementing corporate strategies for overall business growth and success
- 
-        We are committed to harnessing the power of technology to boost business efficiency, reduce costs, and save time. By staying at the forefront of IT 
-        advancements, we offer tailored solutions that optimize operational performance and create long-term value. Our mission is to guide organizations 
-        through today’s complexities while preparing them for future success in an ever-changing digital world.
+      </p>
+      <ul>
+        <li>Providing telecommunication and emerging wireless communication solutions</li>
+        <li>Offering digital transformation services to modernize and enhance operations</li>
+        <li>Assisting organizations in implementing Artificial Intelligence for competitive advantage</li>
+        <li>Deploying cybersecurity solutions to safeguard critical business data</li>
+        <li>Developing and implementing corporate strategies for overall business growth and success</li>
+      </ul>
+      <p>
+        We are committed to harnessing the power of technology to boost business efficiency, reduce costs, and save time. By staying at the forefront of IT advancements, we offer tailored solutions that optimize operational performance and create long-term value. Our mission is to guide organizations through today’s complexities while preparing them for future success in an ever-changing digital world.
       </p>
     </div>
   );
